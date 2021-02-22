6c3e8566c0a7d7ffcb727746fe3d6a3ec804f300
maneesha14w <maneesha14w@gmail.com>
2021-02-22 13:09:16 +0530

    generates a random image.

--- app/src/main/java/com/maneesha14w/speedguess/Identify_Car_Make_Activity.java
+++ app/src/main/java/com/maneesha14w/speedguess/Identify_Car_Make_Activity.java
@@ -3,12 +3,27 @@ package com.maneesha14w.speedguess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class Identify_Car_Make_Activity extends AppCompatActivity {
+    private final String[] carNames = {"ford", "mercedes", "tesla"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify__car__make_);
+
+        ImageView imgView = findViewById(R.id.car_img_view);
+        imgView.setImageResource(getRandomImageResource());
+    }
+
+    private int getRandomImageResource() {
+        Random rand = new Random();
+        String fileNum = String.valueOf(rand.nextInt(10) + 1);
+        int carNameNum = rand.nextInt(3);
+        String fileName = carNames[carNameNum] + "_" + fileNum;
+        return getResources().getIdentifier(fileName, "drawable", getPackageName());
     }
 }
