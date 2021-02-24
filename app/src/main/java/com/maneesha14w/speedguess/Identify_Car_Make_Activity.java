b0eb93f7db36b4463596c2cadcc53c9a5acdab6f
maneesha14w <maneesha14w@gmail.com>
2021-02-24 19:31:29 +0530

    added tag to imageview to see correct answer.

--- app/src/main/java/com/maneesha14w/speedguess/Identify_Car_Make_Activity.java
+++ app/src/main/java/com/maneesha14w/speedguess/Identify_Car_Make_Activity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -21,15 +22,16 @@ public class Identify_Car_Make_Activity extends AppCompatActivity {
 
         // getting and setting the image view to a random img
         ImageView imgView = findViewById(R.id.car_img_view);
-        imgView.setImageResource(getRandomImageResource());
+        imgView.setImageResource(getRandomImageResource(imgView));
     }
 
     //method that returns int id of a random image
-    private int getRandomImageResource() {
+    private int getRandomImageResource(ImageView imgView) {
         Random rand = new Random();
         String fileNum = String.valueOf(rand.nextInt(10) + 1); //Random car number
-        int carNameNum = rand.nextInt(3); // random index from car names array
-        String fileName = carNames[carNameNum] + "_" + fileNum; //string that corresponds to a random image file
+        String carName = carNames[rand.nextInt(3)]; // random index from car names array
+        imgView.setTag(carName); //setting the tag of the ImageView to whatever make of car is selected
+        String fileName = carName + "_" + fileNum; //string that corresponds to a random image file
         Log.d("FILE_NAME", fileName); // log to check if any errors with any file
         return getResources().getIdentifier(fileName, "drawable", getPackageName()); // returns an int id of the filename
     }
