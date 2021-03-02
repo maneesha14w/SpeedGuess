9e79543faff55cbe4791e62a544107199b05d0da
maneesha14w <maneesha14w@gmail.com>
2021-03-02 23:29:03 +0530

    next button works.

--- app/src/main/java/com/maneesha14w/speedguess/Hints_Activity.java
+++ app/src/main/java/com/maneesha14w/speedguess/Hints_Activity.java
@@ -2,6 +2,7 @@ package com.maneesha14w.speedguess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,6 +25,8 @@ public class Hints_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_hints_);
 
         ImageView imgView = findViewById(R.id.car_img_view);
+        Button submitBtn = findViewById(R.id.submitBtnHint);
+        submitBtn.setTag("");
         setImage(imgView);
         setDashes(imgView);
     }
@@ -50,32 +53,48 @@ public class Hints_Activity extends AppCompatActivity {
     }
 
     public void addCharClick(View view) {
-        EditText editText = findViewById(R.id.charTextBox);
-        ImageView imgView = findViewById(R.id.car_img_view);
-        TextView textView = findViewById(R.id.dashTextView);
-        Button submitBtn = findViewById(R.id.identify_btn);
+        Button submitBtn = findViewById(R.id.submitBtnHint);
+        if (submitBtn.getTag().equals("next")) {
+            Intent intent = new Intent(Hints_Activity.this, Hints_Activity.class);
+            finish();
+            overridePendingTransition(1, 0);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+        } else {
+            EditText editText = findViewById(R.id.charTextBox);
+            ImageView imgView = findViewById(R.id.car_img_view);
+            TextView textView = findViewById(R.id.dashTextView);
 
-        String correctModel = imgView.getTag().toString();
-        String newStr = imgView.getTag().toString();
-        String enteredChar = editText.getText().toString();
 
-        int i = correctModel.indexOf(enteredChar);
-        if (i != -1) {
-            list.add(enteredChar);
-            String concatStr = "";
+            String correctModel = imgView.getTag().toString();
+            String newStr = imgView.getTag().toString();
+            String enteredChar = editText.getText().toString();
 
-            for (String e : list) {
-                concatStr = concatStr + e;
+            int i = correctModel.indexOf(enteredChar);
+            if (i != -1) {
+                list.add(enteredChar);
+                String concatStr = "";
+
+                for (String e : list) {
+                    concatStr = concatStr + e;
+                }
+
+                newStr = newStr.replaceAll("[^" + concatStr + " ]", " _ ");
+                if (newStr.equals(correctModel)) {
+                    Toast toast = Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT);
+                    toast.getView().setBackgroundColor(getResources().getColor(R.color.green));
+                    toast.show();
+
+                    submitBtn.setText(R.string.next_btn_txt);
+                    submitBtn.setTag("next");
+                    editText.setEnabled(false);
+                }
+
+                textView.setText(newStr);
+            } else {
+                Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
             }
-
-            newStr = newStr.replaceAll("[^" + concatStr + " ]", " _ ");
-            if (newStr.equals(correctModel)) {
-                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
-            }
-            textView.setText(newStr);
-        }
-        else {
-            Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
+            editText.getText().clear();
         }
     }
 }
--- app/src/main/java/com/maneesha14w/speedguess/MainActivity.java
+++ app/src/main/java/com/maneesha14w/speedguess/MainActivity.java
@@ -3,11 +3,15 @@ package com.maneesha14w.speedguess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
