d26defab1d850caadddd13047ca9566374c6c0da
Maneesha <maneesha14w@gmail.com>
2021-03-03 23:11:14 +0530

    Identify image activity added

---
+++ app/src/main/java/com/maneesha14w/speedguess/Identify_Image_Activity.java
@@ -0,0 +1,14 @@
+package com.maneesha14w.speedguess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Identify_Image_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identify__image_);
+    }
+}
